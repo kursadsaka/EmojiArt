8663b31b23e3dc9178bda73b4381261f2e4bf659
Kürşad Saka <kursadsakaa@gmail.com>
2022-03-02 03:13:16 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Kürşad Saka on 2.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
