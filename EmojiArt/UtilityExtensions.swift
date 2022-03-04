688bbd59a9cae169373f169c881903a7ad0aeec4
Kürşad Saka <kursadsakaa@gmail.com>
2022-03-05 01:23:22 +0300

    add drag and drop

--- EmojiArt/UtilityExtensions.swift
+++ EmojiArt/UtilityExtensions.swift
@@ -18,3 +18,54 @@ extension CGRect {
         CGPoint(x: midX, y: midY)
     }
 }
+
+extension Character {
+    var isEmoji: Bool {
+        if let firstScalar = unicodeScalars.first, firstScalar.properties.isEmoji {
+            return (firstScalar.value >= 0x238d || unicodeScalars.count > 1)
+        }
+        else {
+            return false
+        }
+    }
+}
+
+extension Array where Element == NSItemProvider {
+    
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        if let provider = first(where: {$0.canLoadObject(ofClass: theType)}) {
+            provider.loadObject(ofClass: theType) {object, error in
+                if let value = object as? T {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        if let provider = first(where: {$0.canLoadObject(ofClass: theType)}) {
+            let _ = provider.loadObject(ofClass: theType) {object, error in
+                if let value = object {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    
+    func loadFirstObject<T>(ofType theType: T.Type, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        loadObjects(ofType: theType, firstOnly: true, using: load)
+    }
+    
+    func loadFirstObject<T>(ofType theType: T.Type, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        loadObjects(ofType: theType, firstOnly: true, using: load)
+    }
+    
+}
